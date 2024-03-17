2451ea46166b9ceb3650ed630ec16feb9edb8d51
rieraa <ameee9@163.com>
2024-03-17 20:53:09 +0800

    ♻️ 优化原生执行代码工具方法

--- src/main/java/com/aneto/tindraojsandbox/util/ProcessUtil.java
+++ src/main/java/com/aneto/tindraojsandbox/util/ProcessUtil.java
@@ -3,54 +3,84 @@ package com.aneto.tindraojsandbox.util;
 import cn.hutool.core.date.StopWatch;
 import cn.hutool.core.text.StrBuilder;
 import com.aneto.tindraojsandbox.model.ExecuteOutPut;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+@Slf4j
 public class ProcessUtil {
 
     public static ExecuteOutPut execute(Process process, String operation) throws InterruptedException, IOException {
+        // 创建返回对象
+        ExecuteOutPut executeOutPut = createExecuteOutPut();
+        // 启动计时器
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
-        ExecuteOutPut executeOutPut = new ExecuteOutPut();
         // 等待编译完成
         int exitValue = process.waitFor();
         executeOutPut.setExitCode(exitValue);
         if (exitValue != 0) {
-            System.out.println(operation + "失败" + exitValue);
-            // 分批获取进程的正常输出
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            StrBuilder strOutPutBuilder = new StrBuilder();
-            // 逐行读取
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                strOutPutBuilder.append(line).append("\n");
-            }
-            // 分批获取进程的错误输出
-            BufferedReader errorBufferReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-            StrBuilder strErrorBuilder = new StrBuilder();
-            // 逐行读取
-            String errorLine;
-            while ((errorLine = errorBufferReader.readLine()) != null) {
-                strErrorBuilder.append(errorLine).append("\n");
-            }
-            executeOutPut.setError(strErrorBuilder.toString());
+            log.error("🚀 ~ file:ProcessUtil.java method:execute line:25 -----operation:" + operation+ "失败");
+            // 获取进程输出和错误输出
+            executeOutPut.setError(getErrorOutput(process));
         } else {
-            System.out.println(operation + "成功");
-
-            // 分批获取进程的正常输出
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            StrBuilder strOutPutBuilder = new StrBuilder();
-            // 逐行读取
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                strOutPutBuilder.append(line).append("\n");
-            }
-            executeOutPut.setOutPut(strOutPutBuilder.toString());
+            log.info("🚀 ~ file:ProcessUtil.java method:execute line:29 -----operation:" + operation+"成功");
+            // 获取进程输出
+            executeOutPut.setOutPut(getOutput(process));
         }
         stopWatch.stop();
         executeOutPut.setTime(stopWatch.getTotalTimeMillis());
         return executeOutPut;
     }
+
+    /**
+     * 创建执行输出对象
+     *
+     * @return executeOutPut 执行输出对象
+     */
+    private static ExecuteOutPut createExecuteOutPut() {
+        ExecuteOutPut executeOutPut = new ExecuteOutPut();
+        executeOutPut.setOutPut("");
+        executeOutPut.setError("");
+        executeOutPut.setTime(0L);
+        executeOutPut.setExitCode(0);
+        return executeOutPut;
+    }
+
+    /**
+     * 获取进程的正常输出
+     * @param process
+     * @return
+     * @throws IOException
+     */
+    private static String getOutput(Process process) throws IOException {
+        StrBuilder strOutPutBuilder = new StrBuilder();
+        // 分批获取进程的正常输出
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            strOutPutBuilder.append(line);
+        }
+        return strOutPutBuilder.toString();
+    }
+
+    /**
+     * 获取进程的错误输出
+     *
+     * @param process
+     * @return
+     * @throws IOException
+     */
+    private static String getErrorOutput(Process process) throws IOException {
+        StrBuilder strErrorBuilder = new StrBuilder();
+        // 分批获取进程的错误输出
+        BufferedReader errorBufferReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        String errorLine;
+        while ((errorLine = errorBufferReader.readLine()) != null) {
+            strErrorBuilder.append(errorLine);
+        }
+        return strErrorBuilder.toString();
+    }
+
 }
